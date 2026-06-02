6fb7a0621626af46edbd382b495e78e164544199
unknown <abrahamperez.itsu@gmail.com>
2026-06-01 22:56:30 -0400

    link actualizado

--- asignacion-03/vite.config.js
+++ asignacion-03/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Reemplaza 'nombre-de-tu-repositorio' por el nombre exacto que le diste o le darás en GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/nombre-de-tu-repositorio/', 
+  base: '/abrahampereztarea/asignacion-03/', 
 })
